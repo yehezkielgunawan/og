3600c8be5da85d058f5fb2030424f22013f6922e
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-02-14 20:44:23 +0700

    style(base.tsx): :lipstick: using tailwind again to style the og image

--- src/pages/api/base.tsx
+++ src/pages/api/base.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { withOGImage } from "next-api-og-image";
 
@@ -12,48 +13,49 @@ export enum GeneralQueryEnum {
 }
 
 export default withOGImage<"query", keyof typeof GeneralQueryEnum>({
+  strategy: "query",
   template: {
-    html: async ({
-      description,
+    react: ({
       logo,
-      theme,
+      description,
       siteName,
+      theme,
       templateTitle,
       logoWidth,
       logoHeight,
     }: any) => {
       const query = {
-        description: description ?? "Description",
-        logo:
-          logo ??
-          "https://res.cloudinary.com/yehez/image/upload/v1636202181/peep_amkhuu.svg",
+        logo: logo ?? "https://res.cloudinary.com/yehez/image/upload/v1636202181/peep_amkhuu.svg",
+        description: description ?? "description",
+        siteName: siteName ?? "siteName",
         theme: theme ?? "dark",
         templateTitle: templateTitle ?? "Template Title",
-        logoWidth: logoWidth ?? "100",
-        siteName: siteName ?? "Site Name",
+        logoWidth: logoWidth ?? 100,
         logoHeight,
-      };
-
-      return `
-        <html>
+      }
+      return (
+        <>
           <head>
-            ${getStyle(query)}
+            <link
+              href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+              rel="stylesheet"
+            />
+            {getStyle(query)}
           </head>
           <body>
-            <div class="container">
-              <img src="${query.logo}" alt="Favicon" />
-                <h1>${query.templateTitle}</h1>
-              <p class="description">${query.description}</p>
-              <h3>${query.siteName}</h3>
+            <div className="flex min-h-screen flex-col items-center justify-center gap-3 container-custom">
+              <img src={logo} alt="logo-image" />
+              <h1 className="mt-2 text-4xl font-semibold">{query.templateTitle}</h1>
+              <p className="text-2xl leading-6 description">{query.description}</p>
+              <h3 className="mt-0.5 text-xl">{query.siteName}</h3>
             </div>
           </body>
-        </html>
-      `;
+        </>
+      );
     },
   },
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getStyle = (
   query: Record<keyof typeof GeneralQueryEnum, string | string[]>
 ) => `
@@ -81,15 +83,7 @@ const getStyle = (
     font-family: 'Inter', sans-serif;
   }
 
-  .container {
-    width: 100vw;
-    height: 100vh;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
+  .container-custom {
     background: ${query.theme === "dark" ? "#222" : "#fff"};
     color: ${query.theme === "dark" ? "white" : "black"};
 
@@ -102,22 +96,11 @@ const getStyle = (
     ${query.logoHeight && `height: ${query.logoHeight}px`}
   }
 
-  h1 {
-    font-size: 3rem;
-    line-height: 1.1;
-    margin-top: 1.5rem;
-  }
-
   h3 {
-    margin-top: 0.5rem;
     color: ${query.theme === "dark" ? "#E5E7EB" : "#374151"};
-    font-size: 1.5rem;
   }
   
   .description {
-    font-size: 1.8rem;
-    line-height: 1.5;
-    margin-top: 1rem;
     color: ${query.theme === "dark" ? "#D1D5DB" : "#1F2937"};
   }
 </style>
